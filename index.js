c0274216a19ee31f41fe771e488c3b114832ed8c
Fadwa <eyryzyey@gmail.com>
2026-03-29 13:29:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
