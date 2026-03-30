495a4ad82c35b670417bf3df5700f753c31e323a
Fadwa <eyryzyey@gmail.com>
2026-03-31 00:18:48 +0100

    Update mediafire.js

--- routes/mediafire.js
+++ routes/mediafire.js
@@ -136,7 +136,7 @@ module.exports = {
   url: `${global.t || "http://localhost:3000"}/api/mediafire-dl/download?url=https://www.mediafire.com/file/nbzh4zoc8ohfrwz/sample.zip/file`,
   logo: "https://www.mediafire.com/favicon.ico",
   category: "download",
-  info: "Get direct download links from MediaFire URLs",
+  info: "Get direct download links from MediaFire URL",
   router
 };
 
